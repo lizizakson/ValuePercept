eb20ab517f094c8434306aec29d45f53e821d402
Liz Izakson <lizizakson@mail.tau.ac.il>
2021-01-14 10:22:19 +0200

    added ROI_analysis

--- code/conf.py
+++ code/conf.py
@@ -23,3 +23,9 @@ BEHAVIOR_PATH = 'Behavior/HCP_behavior_includeZscores_V2.csv'
 
 #These paths are for the Plot Results step (required to have the Model outputs):
 RESULTS_DIR = '/mnt/c/Users/liz/Contacts/Desktop/ValuePercept/results/'
+
+#Paths for ROI analyses:
+## Value network paths:
+VALUE_MASK_PATH = BASE_DIR + 'ROI_analysis/1-s2.0-S1053811913002188-mmc2/nifti_masks/fig09/'
+vmPFC_mask_file = VALUE_MASK_PATH + 'binConjunc_PvNxDECxRECxMONxPRI_striatum.nii.gz'
+STR_mask_file = VALUE_MASK_PATH + 'binConjunc_PvNxDECxRECxMONxPRI_vmpfc.nii.gz'
