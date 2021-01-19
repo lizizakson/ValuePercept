bb859935d818bb95418eee277e0ede2dc8c73ecf
Liz Izakson <lizizakson@mail.tau.ac.il>
2021-01-19 10:41:47 +0200

    Added ROI analysis

--- code/conf.py
+++ code/conf.py
@@ -29,3 +29,4 @@ RESULTS_DIR = '/mnt/c/Users/liz/Contacts/Desktop/ValuePercept/results/'
 VALUE_MASK_PATH = BASE_DIR + 'ROI_analysis/1-s2.0-S1053811913002188-mmc2/nifti_masks/fig09/'
 vmPFC_mask_file = VALUE_MASK_PATH + 'binConjunc_PvNxDECxRECxMONxPRI_striatum.nii.gz'
 STR_mask_file = VALUE_MASK_PATH + 'binConjunc_PvNxDECxRECxMONxPRI_vmpfc.nii.gz'
+
