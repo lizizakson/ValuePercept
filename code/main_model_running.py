f3735d5f7b235ba6197e81061da3be6de264f5c2
Liz Izakson <lizizakson@mail.tau.ac.il>
2021-06-23 10:33:43 +0300

    Added split according to family

--- code/main_model_running.py
+++ code/main_model_running.py
@@ -39,34 +39,64 @@ import conf
 import importlib
 
 
-def main():
+def main(cross = False, split_fam = True):
     #load data for model
-    df = pd.read_csv('df_behav.csv')
-    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    df = pd.read_csv('df_behav_fam.csv', index_col=0) #subject ID as index column
     print(df.shape)
-    all_fc_data = pd.read_csv('all_fc_data.csv')
-    all_fc_data = all_fc_data.loc[:, ~all_fc_data.columns.str.contains('^Unnamed')]
+    all_fc_data = pd.read_csv('all_fc_data.csv', index_col=0) #subject ID as index column
     print(all_fc_data.shape)
     
-    #pre-provessing
-    params_dict = {'X_preprocess_type': 'fisher_z', 'y_preprocess_type': None, 'test_size': 0.2}
-    behav_name = "DDisc_AUC_40K"
+    params_dict = {'X_preprocess_type': 'z_score', 'y_preprocess_type': None, 'test_size': 0.2}
+    #df['rank_compare_log'] = np.log(df['rank_compare'])
+    #df['rank_compare_boxcox'], fitted_lambda = stats.yeojohnson(df['rank_compare'])
+    behav_name = "Flanker_AgeAdj"
+
+        
+
+    #make sure that the neural data (all_fc_data) and the behavioral data are the same length
+    if df[behav_name].isnull().sum() > 0: #if there are missing values in the behavioral measurement
+        df = df.dropna(subset=[behav_name]) #exclude the null score from the df
+
+        #equalize the subjects in the behav data and the fc data
+        subject_IDs = all_fc_data.index
+        subjectBehavior = df.index
+        print("Before equalization:")
+        print("Number of subjects in x_features is {0}".format(len(subject_IDs)))
+        print("And, the number of subjects in the behavior data is {0}".format(len(subjectBehavior)))
+
+        subjectDifference = np.setdiff1d(subject_IDs, subjectBehavior)
+        print("So, the difference between the two data sets is {0}".format(len(subjectDifference)))
+
+        #make sure the difference is smaller than 5
+        assert len(subjectDifference) < 5, "large difference between behav and FC data sets"
+
+        all_fc_data = all_fc_data.drop(subjectDifference)
+    
+    #Make sure the subject lists in the fMRI data and behavior data are equal
+    assert all_fc_data.index.equals(df.index), "Row (subject) indices of FC vcts and behavior don't match!"
+        
     behav = df[behav_name] #send the behavioral score: DDisc_AUC_40K, Flanker_AgeAdj, rank_compare
 
-    reg_flanker = Model('ElasticNet', params_dict, all_fc_data, behav)
+    #pre-processing
+    reg_flanker = Model('ElasticNet', params_dict, all_fc_data, behav, df)
     reg_flanker.preprocess()
-    reg_flanker.split_train_test(dbg = False)
+
+    if split_fam:
+        reg_flanker.split_train_test_fam(dbg = True)
+    else:
+        reg_flanker.split_train_test(dbg = False)
 
     #fit
     params_run = {'params_selec': {"selection_method": "univariate", "corr_type": "pearson", "percent": 0.05}, 
-                    'params_fit': {'l1_ratio': 0.01, 'alpha': [1e-4, 1e-3, 1e-2, 1e-1, 0.0, 1.0, 10.0]},
+                    'params_fit': {'l1_ratio':  [0.01], 'alpha': [1e-4, 1e-3, 1e-2, 1e-1, 0.0, 1.0, 10.0]},
                     'params_predict': {'eval_score': 'mean_squared_error', 'best_params': None}}
     
     final_model, final_model_score, yhat, rel_features, rel_features_temp = reg_flanker.fit_model(override_params = params_run)
     print(final_model)
 
     #output
-    colors_dict = {'DDisc_AUC_40K': '#5eaaa8', 'Flanker_AgeAdj': "#eb5e0b", 'rank_compare': "#4a47a3", 'control_task': "#FF3659"}
+    colors_dict = {'DDisc_AUC_40K': '#5eaaa8', 'Flanker_AgeAdj': "#eb5e0b", 'rank_compare_boxcox': "#4a47a3", 
+                    'PicVocab_AgeAdj': "#FF3659", "Mars_Final": "#808080"}
     parc_file_name = "Schaefer2018_100Parcels_7Networks_order_info.txt"
     
     reg_flanker.plot_output(yhat, colors_dict[behav_name]) #color according to the specific predicted task scores
@@ -77,19 +107,20 @@ def main():
     #print(reg_flanker.rel_features)
 
     #cross_model
-    behav_name = "Flanker_AgeAdj"
-    behav = df[behav_name]
-    reg_DD = Model('ElasticNet', params_dict, all_fc_data, behav)
-    reg_DD.set_rel_features(reg_flanker.rel_features)
-    reg_DD.preprocess()
-    reg_DD.split_train_test(dbg = False)
-    #fit
-    params_run = {'params_fit': {'l1_ratio': 0.01, 'alpha': [1e-4, 1e-3, 1e-2, 1e-1, 0.0, 1.0, 10.0]},
-                    'params_predict': {'eval_score': 'mean_squared_error', 'best_params': None}}
-    
-    final_model, final_model_score, yhat = reg_DD.fit_model(override_params = params_run, select_features = False)
-    print(final_model)
-    reg_DD.plot_output(yhat, colors_dict[behav_name])
+    if cross:
+        behav_name = "DDisc_AUC_40K"
+        behav = df[behav_name]
+        reg_DD = Model('ElasticNet', params_dict, all_fc_data, behav)
+        reg_DD.set_rel_features(reg_flanker.rel_features)
+        reg_DD.preprocess()
+        reg_DD.split_train_test(dbg = False)
+        #fit
+        params_run = {'params_fit': {'l1_ratio': np.arange(0, 1, 0.01), 'alpha': [1e-4, 1e-3, 1e-2, 1e-1, 0.0, 1.0, 10.0]},
+                        'params_predict': {'eval_score': 'mean_squared_error', 'best_params': None}}
+        
+        final_model, final_model_score, yhat = reg_DD.fit_model(override_params = params_run, select_features = False)
+        print(final_model)
+        reg_DD.plot_output(yhat, colors_dict[behav_name])
 
 if __name__ == "__main__":
     main()
